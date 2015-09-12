bff99d496d474a9bf6d628600a25f7762eb73c71
realsoc <hugo.djemaa@gmail.com>
2015-09-12 18:33:45 +0200

    done

--- ImageAndPDFParser.php
+++ ImageAndPDFParser.php
@@ -3,19 +3,22 @@ require_once('RemoteObject.php');
 require_once('RemoteRecipe.php');
 
 class ImageAndPDFParser {
-		private static $apiURL = "http://smw.learning-socle.org/api.php?";
-		private static $query = "action=query&prop=imageinfo&titles=Image:%TITRE%&iiprop=url&format=json";
-
-		public static function getJson($object){
+		private static $apiURL = "/api.php?";
+		private static  $query = "action=query&prop=imageinfo&titles=Image:%TITRE%&iiprop=url&format=json";
+		private $mn;
+		public function __construct(){
+			$this->mn = new Login;
+		}
+		public function getJson($object){
 			$mQuery = urlencode($object->getQuery());
 			$url= ImageAndPDFParser::$apiURL.ImageAndPDFParser::$query;
-			return file_get_contents($url);
+			return $this->mn->callApi($url);
 		}
-		public static function getURL($name){
-			$jsonString = ImageAndPDFParser::getObjectAsJson($name);
-			return ImageAndPDFParser::retrieveUrlFromObject(json_decode($jsonString, true));
+		public function getURL($name){
+			$jsonString = $this->getObjectAsJson($name);
+			return $this->retrieveUrlFromObject(json_decode($jsonString, true));
 		}
-		private static function retrieveUrlFromObject($object){
+		private function retrieveUrlFromObject($object){
 			$ret = 'http://www.404notfound.fr/assets/images/pages/img/kaspcreationsdotnl.jpg';
 			$result =  $object["query"]["pages"];
 			if(!array_key_exists("-1", $result)){
@@ -24,10 +27,10 @@ class ImageAndPDFParser {
 			}
 			return $ret;
 		}
-		private static function getObjectAsJson($name){
+		private function getObjectAsJson($name){
 			$url=ImageAndPDFParser::$apiURL.ImageAndPDFParser::$query;	
 			$url = str_replace('%TITRE%', $name, $url);		
-			return file_get_contents($url);
+			return $this->mn->callApi($url);
 		}
 	}
 ?>
